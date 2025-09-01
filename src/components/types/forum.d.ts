bfadf19a45c6280fe7d21f59035bc2f8e30261f9
Rebecka Larsson <rebecka.rl.larsson@gmail.com>
2025-09-01 11:35:59 +0200

    Basic types added

---
+++ src/components/types/forum.d.ts
@@ -0,0 +1,28 @@
+type ThreadCategory = "THREAD" | "QNA";
+
+type User = {
+	userName: string;
+	password: string
+}
+
+type Thread = {
+  id: number;
+	title: string;
+	category: ThreadCategory;
+	creationDate: string;
+	description: string;
+	creator: User;
+}
+
+type QNAThread =  Thread & { //Type extension
+	category: "QNA";
+	isAnswered: boolean;
+	commentAnswerId?: number;
+}
+
+type Comment = {
+	id: number;
+	thread: number;
+	content: string;
+	creator: User
+}
